978e998d00fcd3460c74314270fda24398c362d2
LOH Yu Jin <yujin.loh.2017@smu.edu.sg>
2019-09-28 01:55:44 +0800

    set dev env

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask;
 app = Flask(__name__)
-import codeitsuisse.routes.square
+from codeitsuisse.routes import square
 
 
-
+app.register_blueprint(square.mod)
--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -1,20 +1,23 @@
 import logging
 import json
 
-from flask import request, jsonify;
+from flask import request, jsonify, Blueprint;
 
 from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
-@app.route('/square', methods=['POST'])
+mod = Blueprint('square', __name__)
+
+@mod.route('/square')
 def evaluate():
-    data = request.get_json();
-    logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input");
-    result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
-    return json.dumps(result);
+    return "square evaluated!"
+    # data = request.get_json();
+    # logging.info("data sent for evaluation {}".format(data))
+    # inputValue = data.get("input");
+    # result = inputValue * inputValue
+    # logging.info("My result :{}".format(result))
+    # return json.dumps(result);
 
 
 
