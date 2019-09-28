dd3964c5503b8efd58b719fa23eece4c01972ed0
Michelle Lion <mflion.2017@accountancy.smu.edu.sg>
2019-09-28 13:43:23 +0800

    Added typing-contest.py with dependency calculate_shortest.py

--- codeitsuisse/routes/calculate_shortest.py
+++ codeitsuisse/routes/calculate_shortest.py
@@ -5,3 +5,14 @@ def calculate_shortest(s1, s2):
             cost += 1
 
     return cost
+
+
+# def find_shortest(s, str_list):
+#     min_trans = 99999999999999
+#     for i in range(len(str_list)):
+#         trans = calculate_shortest(s, str_list[i])
+#         if trans < min_trans:
+#             min_trans = trans
+#             min_str = str_list[i]
+
+#     return (min_trans, min_str)
