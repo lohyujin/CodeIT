f50ff3b386877ef01f33d8d0c01de9fdf6f7ef6a
Michelle Lion <mflion.2017@accountancy.smu.edu.sg>
2019-09-28 15:50:28 +0800

    Modified defuse.py

--- codeitsuisse/routes/defuse.py
+++ codeitsuisse/routes/defuse.py
@@ -14,26 +14,29 @@ def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
 
-    n = data['n']
-    k = data['k']
-    password = data['password']
+    result = []
+    for row in data:
+        n = row.get('n')
+        k = row.get('k')
+        password = row.get('password')
 
-    result = 0
+        total = 0
 
-    if n >= 3:
-        for sub in range(3, n+1, 2):
-            for i in range(n-sub+1):
-                sub_array = password[i:i+sub]
-                if sub_array == sub_array[::-1]:
-                    num_rand = sub_array.count(-1)
-                    if num_rand % 2 == 0:
-                        num = num_rand // 2
-                    else:
-                        num = num_rand // 2 + 1
+        if n >= 3:
+            for sub in range(3, n+1, 2):
+                for i in range(n-sub+1):
+                    sub_array = password[i:i+sub]
+                    if sub_array == sub_array[::-1]:
+                        num_rand = sub_array.count(-1)
+                        if num_rand % 2 == 0:
+                            num = num_rand // 2
+                        else:
+                            num = num_rand // 2 + 1
 
-                    result += k ** num 
+                        total += k ** num 
     
-    result = [result % 998244353]
+        total = total % 998244353
+        result.append(total)
 
     logging.info("My result :{}".format(result))
-    return jsonify(['result': result])
+    return jsonify(result=result)
